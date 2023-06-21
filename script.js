850fbb9ccead982999ce69b9d79e034b025c795b
norkitorki <norki@tuta.io>
2023-06-21 07:10:56 +0200

    Define playerSelection()

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function getComputerChoice() {
   const index = Math.floor(Math.random() * 3);
   return CHOICES[index];
 };
+
+function playerSelection() {
+  let instructions = `Please input your choice:\n1. ${CHOICES[0]}\n2. ${CHOICES[1]}\n3. ${CHOICES[2]}\n\nOr input 'rock', 'paper', or 'scissors'.`, choice;
+
+  while (!CHOICES.includes(choice)) {
+    choice = prompt(instructions);
+    choice = CHOICES[Number(choice) - 1] || CHOICES.find(el => el.toLowerCase() === choice.toLowerCase());
+  }
+
+  return choice;
+};
