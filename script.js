37ddb42026b35d7ddbb9199cad4081706c017596
norkitorki <norki@tuta.io>
2023-06-21 07:10:40 +0200

    Define getComputerChoice()

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const startButton = document.querySelector('.start-game');
 const CHOICES = [ 'Rock', 'Paper', 'Scissors' ];
+
+function getComputerChoice() {
+  const index = Math.floor(Math.random() * 3);
+  return CHOICES[index];
+};
